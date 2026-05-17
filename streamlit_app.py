bdc66f7bd0760c2499c2223db6ced4568dc12946
kathy0716-h-afk <khashimoto@go.gsis.kumamoto-u.ac.jp>
2026-05-17 21:03:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -171,7 +171,7 @@ def update_data(conn, df, changes):
 
 # Set the title that appears at the top of the page.
 """
-# :shopping_bags: Inventory tracker
+# 🛍️ Inventory tracker
 
 **Welcome to Alice's Corner Store's intentory tracker!**
 This page reads and writes directly from/to our inventory database.
